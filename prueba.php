fda7b247365e754dd723156620f9fb047c730096
Cecilia Lombart <cecilia.lombart@gmail.com>
2021-11-28 14:00:15 -0300

    primer commit

---
+++ prueba.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Actividad Obligatoria U2</title>
+</head>
+<body>
+
+    <?php
+
+        $array = ["Cecilia","Lombart", "31", "Tocar la guitarra", "VSC", "Windows 10"];
+        for ($i = 0; $i<5; $i++){
+            echo $array [$i];
+            echo"<br>";
+            echo"<br>";
+        }
+ 
+    ?>
+    
+</body>
+</html>
